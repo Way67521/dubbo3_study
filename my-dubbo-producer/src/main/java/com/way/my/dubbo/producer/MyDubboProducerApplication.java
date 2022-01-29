f200548b5a524f6af753fd885409e69f0e68d7de
Way67521 <ga3372186@gmail.com>
2022-01-29 15:40:26 +0800

    init project!

---
+++ my-dubbo-producer/src/main/java/com/way/my/dubbo/producer/MyDubboProducerApplication.java
@@ -0,0 +1,13 @@
+package com.way.my.dubbo.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDubboProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDubboProducerApplication.class, args);
+    }
+
+}
---
+++ my-dubbo-service/src/main/java/com/way/my/dubbo/service/MyDubboServiceApplication.java
@@ -0,0 +1,13 @@
+package com.way.my.dubbo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDubboServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDubboServiceApplication.class, args);
+    }
+
+}
