028b25cf4c527c7592cc4bc22b97af00e8a20380
way <ga3372186@gmail.com>
2022-01-29 15:35:52 +0800

    init projects!

---
+++ my-dubbo-consumer/src/main/java/com/way/my/dubbo/consumer/MyDubboConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.way.my.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDubboConsumerApplication.class, args);
+    }
+
+}
---
+++ my-dubbo-service/src/test/java/com/way/my/dubbo/service/MyDubboServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.way.my.dubbo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDubboServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
